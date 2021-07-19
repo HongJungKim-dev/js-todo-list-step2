8a3adc5165612441730a9eab1af4d8f96781a953
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-07-19 19:07:17 +0900

    feat: 사용자 생성(api연동x)
    사용자 버튼을 눌러 사용자를 추가합니다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,54 @@
+const userName = 'hong'
+
+const titleUI = document.getElementById('user-title');
+
+titleUI.innerHTML = `<span><strong>${userName}</strong>'s Todo List</span>`;
+
+const URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api';
+
+const userListUI = document.getElementById('user-list');
+
+const users = [];
+
+users.push(`<button class="ripple active">
+    ${userName}
+  </button>`);
+
+const controlBUtton = `<button class="ripple user-create-button" data-action="createUser">
+  + 유저 생성
+  </button>
+  <button class="ripple user-delete-button" data-action="deleteUser">
+  삭제 -
+  </button>
+  `
+userListUI.innerHTML = users.join('') + controlBUtton;
+
 const onUserCreateHandler = () => {
-  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  const userName = prompt('추가하고 싶은 이름을 입력해주세요.');
+
+  users.push(` <button class="ripple active">
+  ${userName}
+  </button>
+  `);
+
+  userListUI.innerHTML = users.join('') + controlBUtton;
+};
+
+const userCreateButton = document.querySelector('.user-create-button');
+userCreateButton.addEventListener('click', onUserCreateHandler);
+
+async function fetchUserList() {
+  const url = `${URL}/users`;
+  const response = await fetch(url);
+  const data = await response.json();
+  
+  return data;
 }
 
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
+function getUserList(){
+  return async() => {
+    const { _id, name, todoList } = await fetchUserList();
+  }
+}
 
+getUserList();
